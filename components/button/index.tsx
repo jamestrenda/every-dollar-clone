0649223ee558cb1a77615e8259cf982872404f27
James Trenda <jamestrenda@gmail.com>
2022-06-10 16:36:30 -0400

    added prop type defs

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link';
+import { ReactNode } from 'react';
 import styled, { css } from 'styled-components';
 import tw from 'twin.macro';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  disabled?: boolean;
+  href?: string;
+  innerStyle?: string; // to pass through additional tailwind class names to the inner html
+  onClick?: () => void;
+  type?: 'button' | 'submit';
+};
 
 const buttonStyles = css`
   ${tw`inline-block bg-black text-white font-bold rounded-full`}
@@ -15,31 +25,31 @@ const StyledLink = styled.a`
   ${buttonStyles}
 `;
 const StyledText = styled.span(
-  ({ textStyles, disabled }: { textStyles: string; disabled: boolean }) => [
+  ({ innerStyle, disabled }: { innerStyle: string; disabled: boolean }) => [
     tw`block py-3 px-6 bg-indigo-500 bg-opacity-100 hover:bg-opacity-0 transition-all rounded-full`,
-    textStyles && textStyles,
+    innerStyle && innerStyle,
     disabled && tw`bg-gray-200`,
   ]
 );
 
-export const Button = (props) => {
-  const { href, children, textStyles } = props;
-
-  const buttonProps = { ...props };
-  delete buttonProps.children;
-  delete buttonProps.textStyles;
+export const Button = (props: ButtonProps) => {
+  const { href, children, innerStyle, onClick } = props;
 
   return href ? (
     <Link href={href} passHref>
       <StyledLink>
-        <StyledText textStyles={textStyles} disabled={props.disabled}>
+        <StyledText innerStyle={innerStyle} disabled={props.disabled}>
           {children}
         </StyledText>
       </StyledLink>
     </Link>
   ) : (
-    <StyledButton {...props}>
-      <StyledText textStyles={textStyles} disabled={props.disabled}>
+    <StyledButton>
+      <StyledText
+        innerStyle={innerStyle}
+        disabled={props.disabled}
+        onClick={onClick}
+      >
         {children}
       </StyledText>
     </StyledButton>
