ccb885a357758087fc1d3478be73f17d4915dcdc
James Trenda <jamestrenda@gmail.com>
2022-06-13 20:28:17 -0400

    move motion up a level so it wraps the layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,25 +51,21 @@ function MyApp({
     <SessionProvider session={session}>
       <ApolloProvider client={client}>
         <ModalStateProvider>
-          {getLayout(
-            <>
-              <motion.div
-                key={router.route}
-                initial="pageInitial"
-                animate="pageAnimate"
-                variants={{
-                  pageInitial: {
-                    opacity: 0,
-                  },
-                  pageAnimate: {
-                    opacity: 1,
-                  },
-                }}
-              >
-                <Component {...pageProps} />
-              </motion.div>
-            </>
-          )}
+          <motion.div
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+          >
+            {getLayout(<Component {...pageProps} />)}
+          </motion.div>
         </ModalStateProvider>
       </ApolloProvider>
     </SessionProvider>
