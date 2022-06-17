e1b660d37101ad90e34e1240aec0c615b10c2d71
James Trenda <jamestrenda@gmail.com>
2022-06-16 20:26:16 -0400

    typo in prod url

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // don't put anything in here that shouldn't be public!
 export const endpoint = 'http://localhost:3000/api/graphql';
-export const prodEndpoint =
-  'https://everydollarclone.jamestrenda.dev/api/graphql';
+export const prodEndpoint = 'https://everydollarclone.trenda.dev/api/graphql';
