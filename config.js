15dbd468e848667ca57ade148d6dfa000b7dcd25
James Trenda <jamestrenda@gmail.com>
2022-06-16 20:10:41 -0400

    update prod graphql api url

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // don't put anything in here that shouldn't be public!
 export const endpoint = 'http://localhost:3000/api/graphql';
-export const prodEndpoint = 'https://app-name.ondigitalocean.app/api/graphql';
+export const prodEndpoint =
+  'https://everydollarclone.jamestrenda.dev/api/graphql';
