3a4d1fa9813b2b9b686dac9c0bc61b857028912f
James Trenda <jamestrenda@gmail.com>
2022-06-17 13:38:58 -0400

    added mobile menu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import { Nav } from '../nav';
 import { UtilityNav } from '../nav/utility';
 import { UserNav } from '../nav/user';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className="flex flex-col min-w-fit h-screen overflow-y-auto xl:w-72">
+    <header
+      className={`!overflow-visible flex flex-col min-w-fit h-screen overflow-y-auto xl:w-72 absolute md:relative top-0 md:top-auto md:bottom-auto md:left-auto bottom-0 -left-[100px] z-[9999] md:z-auto bg-gray-100 transition-all ${
+        menuOpen ? '!left-0 shadow-md' : ''
+      }`}
+    >
       {/* <Link href="/"> */}
       <div className="grid place-items-center text-5xl p-5 text-green-400">
         <RiExchangeDollarFill />
@@ -18,6 +25,16 @@ const Header = () => {
         <UtilityNav />
         <UserNav />
       </div>
+      <button
+        type="button"
+        onClick={() => setMenuOpen(!menuOpen)}
+        className={`absolute bottom-5 -right-[4.5rem] text-white p-4 rounded-full  md:hidden text-xl shadow-md leading-[1] ${
+          menuOpen ? 'bg-black ' : 'bg-indigo-500'
+        }`}
+        title={menuOpen ? 'Close Menu' : 'Open Menu'}
+      >
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </button>
     </header>
   );
 };
