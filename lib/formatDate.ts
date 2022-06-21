7ef93867da882747c0f95284c47d365278504fef
James Trenda <jamestrenda@gmail.com>
2022-06-21 18:41:09 -0400

    revert to prev code due to error on main trans btn

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,7 +1,5 @@
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 
 export function formatDate(date: Date | number | string, token?: string) {
-  let d = new Date(date);
-  if (typeof date === 'string') d = parseISO(date);
-  return format(d, token ?? 'M/d/yy');
+  return format(new Date(date), token ?? 'M/d/yy');
 }
